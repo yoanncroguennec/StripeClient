6d0f1dfbc83ca902116554cb51052bd264c66abe
sido69 <yoann.croguennec@gmail.com>
2023-03-10 13:03:22 +0100

    first commit

--- src/CheckoutForm.jsx
+++ src/CheckoutForm.jsx
@@ -10,32 +10,24 @@ export default function CheckoutForm() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const cardElement = elements.getElement(CardElement);
+    const stripeToken = await stripe.createToken(cardElement, {
+      name: "Bibou",
+    });
 
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-        type: "card",
-        card: elements.getElement(CardElement)
-    })
-    if(!error){
-        console.log("Token généré : ", paymentMethod);
-        try {
-            const { id } = paymentMethod
-            const response = await axios.post(
-              `http://localhost:8080/api/payment`,
-              {
-                amount: 100,
-                id: id,
-              }
-            );
-            if (response.data.success) {
-                console.log("Payement réussi");
-            }
-        } catch (error) {
-            console.log("Erreur", error);
-        }
-    } else {
-        console.log(error.message);
+    console.log("Token généré : ", stripeToken.token.id);
+    try {
+      const response = await axios.post(`http://localhost:8080/api/payment`, {
+        amount: 100,
+        stripeToken: stripeToken.token.id,
+      });
+      if (response.data.success) {
+        console.log("Payement réussi");
+      }
+    } catch (error) {
+      console.log("Erreur", error);
     }
-}
+  };
 
   return (
     <form style={{ maxWidth: "400px" }} onSubmit={handleSubmit}>
@@ -46,9 +38,7 @@ export default function CheckoutForm() {
         }}
       />
 
-      <button type="submit">
-          Payer
-        </button>
+      <button type="submit">Payer</button>
     </form>
   );
 }
